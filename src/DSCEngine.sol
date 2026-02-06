53b29c098ce248d290dc185792a020449d6ccb03
Nwabuike Noble <nwabuikenoble@gmail.com>
2026-02-06 14:17:00 +0100

    Oracle lib

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -26,6 +26,8 @@ import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {OracleLib} from "./libraries/OracleLib.sol";
+
 /**
  * @title DSCEngine
  * @author Nwabuike Noble
@@ -56,6 +58,9 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__MintFailed();
     error DSCEngine__HealthFactorOk();
     error DSCEngine__HealthFactorNotImproved();
+
+    //Types
+    using OracleLib for AggregatorV3Interface;
 
     //State Variables
     uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
